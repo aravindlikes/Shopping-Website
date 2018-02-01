b12cc5866a0fd612e48a62df9b41639a7a2e5ef5
Aravind <aravindanarayanan.m@gmail.com>
2018-02-01 16:30:19 +0530

    Update cred.php

--- includes/cred.php
+++ includes/cred.php
@@ -1,6 +1,6 @@
 <?php
 	$mail_id="aravindanarayanan.m@gmail.com";
-	$mail_pass="aravindlove";
+	$mail_pass="thinksomething";
 
 	$Insta_API_KEY="798af47cf56c915dcd2afa5e4602e3eb";
 	$Insta_AUTH_TOKEN="18ea47c9d70c609d09d7113370e3feca";
